b46d94856167a3756cd2498e10c5e23864eab9ef
Higor Camelo <higorcamelo@gmail.com>
2024-04-02 20:09:11 -0300

    Amanhã mexo mais

---
+++ telegram_bot.py
@@ -0,0 +1,9 @@
+import logging
+import pytz 
+import datetime
+from telegram import Update
+from telegram.ext import Application, CommandHandler, CallbackContext, JobQueue, MessageHandler, filters
+
+# Initialize logging
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=config.logging_level, filename= config.logging_file)
+logging.getLogger("httpx").setLevel(config.logging_level)
